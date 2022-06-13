da9d909c6798f901cbe526a6f00994276e2197bb
ayaroshynska <a.yaroshynska@gethatch.com>
2022-06-13 23:17:18 +0300

    update

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ base_headers = {
     'User_Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36'
 }
 logger = logging.getLogger('Petlyura')
-logging.basicConfig(level=logging.ERROR)
+logging.basicConfig(level=logging.INFO)
 
 def process_input(filename) -> Dict: 
     with open(filename, 'r') as input_file:
@@ -55,8 +55,9 @@ def fetch_data(keywords: List, proxy_list: List = None, type: AnyStr = None) -> 
         if repo_list:
             repo_info = []
             for elem in repo_list:
+                link = parse.urljoin(base_link, elem)
                 author = elem.split('/')[1]
-                repo_resp = session.get(parse.urljoin(base_link, elem), headers=base_headers)
+                repo_resp = session.get(link, headers=base_headers)
                 tree = html.fromstring(repo_resp.text)
                 languages = tree.xpath('//span[@class="Progress"]/span')
                 language_stats = {}
@@ -75,4 +76,10 @@ def fetch_data(keywords: List, proxy_list: List = None, type: AnyStr = None) -> 
 
 if __name__ == "__main__":
     with open ('output.json', 'w') as out_file:
-        out_file.write(json.dumps(process_input(filename='input.json')))
+        logger.info("Let's crawl some Github content! Starting...")
+        result = process_input(filename='input.json')
+        if result:
+            out_file.write(json.dumps(result))
+            logger.info("Success! Enjoy your data!")
+        else:
+            logger.info("Oops, something gone wrong. Check logs below!")
--- tests.py
+++ tests.py
@@ -1,9 +1,20 @@
 import unittest
+import json
 
 from main import process_input
 
 class TestDataType(unittest.TestCase):
+    
+    def test_list_input(self):
+        filename = 'resources/list_input_test.json'
+        with open(filename, 'r') as input:
+            self.assertIsInstance(json.loads(input.read()), list)
         
+    def test_dict_input(self):
+        filename = 'resources/dict_input_test.json'
+        with open(filename, 'r') as input:
+            self.assertIsInstance(json.loads(input.read()), dict)
+            
     def test_input_empty(self):
         filename = 'resources/empty_input_test.json'
         with self.assertLogs('Petlyura', level='ERROR') as cm:
